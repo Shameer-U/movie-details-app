94919d12377bd8719365e6b3c2ba2997a09e02ae
Shameer-U <shameerushahul@gmail.com>
2022-07-17 16:11:23 +0530

    show listing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,24 +3,29 @@ import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
 import { APIKey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from 'react-redux';
-import { addMovies } from '../../features/movies/movieSlice';
+import { addMovies, fetchAsyncMovies, fetchAsyncShows} from '../../features/movies/movieSlice';
 
 const Home = () => {
-  const movieText = "Harry";
+  //const movieText = "Harry";
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-        const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log("Err :", err);
-        });
+  // useEffect(() => {
+  //   const fetchMovies = async () => {
+  //       const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+  //       .catch((err) => {
+  //         console.log("Err :", err);
+  //       });
         
-        dispatch(addMovies(response.data));
-    }
+  //       dispatch(addMovies(response.data));
+  //   }
 
-    fetchMovies();
-  }, []);
+  //   fetchMovies();
+  // }, []);
+
+  useEffect(() => {
+      dispatch(fetchAsyncMovies());
+      dispatch(fetchAsyncShows());
+  }, [dispatch]);
 
   return (
     <div>
